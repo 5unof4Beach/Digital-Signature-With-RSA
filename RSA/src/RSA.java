6c6b8582a55bb82592e23505ea193b5e05be031d
5unof4Beach <suckmydick2001gta@gmail.com>
2022-03-06 23:00:17 +0700

    test

--- RSA/src/RSA.java
+++ RSA/src/RSA.java
@@ -1,7 +1,20 @@
 package RSA.src;
 
+import java.math.BigInteger;
+
+import java.util.*;
+
 public class RSA {
     public static void main(String[] args) {
-        System.out.println("TEST");   
+        print(1);
+    }
+
+    public static void print(int n){
+        System.out.println(n);       
+    }
+
+    public static void print(String s){
+        System.out.println(s);       
     }
 }
+
