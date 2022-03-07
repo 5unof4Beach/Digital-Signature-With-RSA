2b50d66f951b74d48f4e3b639c4f56bd04869459
Duc Bui <39622487+5unof4Beach@users.noreply.github.com>
2022-03-07 22:22:56 +0700

    minor fix

--- RSA/src/rsa/Demo.java
+++ RSA/src/rsa/Demo.java
@@ -2,11 +2,13 @@
 package rsa;
 
 import java.math.BigInteger;
+import java.util.*;
 
 public class Demo {
     public static void main(String[] args) {
         RSA r = new RSA();
-        BigInteger mess = new BigInteger("21346567");
+        
+        BigInteger mess = new BigInteger(new Scanner(System.in).nextLine());
         
         System.out.printf("public key: (%s, %s)\n",r.getN().toString(), r.getE().toString());
         System.out.printf("private key: (%s, %s)\n",r.getN().toString(), r.getD().toString());
